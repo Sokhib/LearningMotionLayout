34ada2e9feaece53b3fa2bd04ad6c678c7524084
Sokhibdzhon <sokhibsaid@gmail.com>
2020-08-08 15:07:20 +0300

    [LMYT] #update #comment Added swipes :)

--- app/src/main/java/com/sokhibdzhon/learningmotionlayout/MainActivity.kt
+++ app/src/main/java/com/sokhibdzhon/learningmotionlayout/MainActivity.kt
@@ -3,9 +3,9 @@ package com.sokhibdzhon.learningmotionlayout
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,32 +19,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        motionLayout = findViewById<MotionLayout>(R.id.motionlayout)
+        motionLayout = findViewById(R.id.motionlayout)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        imageview_true.setOnClickListener(null)
-        imageview_false.setOnClickListener(null)
-        imageview_pass.setOnClickListener(null)
-        imageview_startGame.setOnClickListener { startGame ->
-            (startGame as ImageView).setImageDrawable(null)
+        viewModel.nextWord()
+        motionLayout.setTransitionListener(object : TransitionAdapter() {
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                super.onTransitionCompleted(motionLayout, currentId)
+                when (currentId) {
+                    R.id.offScreenCorrect,
+                    R.id.offScreenInCorrect -> {
+                        motionLayout!!.progress = 0f
+//                        motionLayout.setTransition(R.id.rest, R.id.offScreenCorrect)
+                        viewModel.nextWord()
+                    }
+                }
+            }
+        })
+        imageview_true.setOnClickListener {
+            Log.d(TAG, "TRUE: Working!!!")
+            startAnimation(it)
             viewModel.nextWord()
-            startAnimation(imageview_pass)
-            startGame.setOnClickListener(null)
-            imageview_true.setOnClickListener {
-                Log.d(TAG, "TRUE: Working!!!")
-                startAnimation(it)
-                viewModel.nextWord()
-            }
-            imageview_false.setOnClickListener {
-                Log.d(TAG, "FALSE: Working!!!")
-                startAnimation(it)
-                viewModel.nextWord()
-            }
-            imageview_pass.setOnClickListener {
-                Log.d(TAG, "PASS: Working!!!")
-                startAnimation(it)
-                viewModel.nextWord()
-            }
-
+        }
+        imageview_false.setOnClickListener {
+            Log.d(TAG, "FALSE: Working!!!")
+            startAnimation(it)
+            viewModel.nextWord()
+        }
+        imageview_pass.setOnClickListener {
+            Log.d(TAG, "PASS: Working!!!")
+            startAnimation(it)
+            viewModel.nextWord()
         }
 
         viewModel.current.observe(this, Observer { currentWord ->
@@ -55,14 +59,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun startAnimation(view: View) {
         Log.d(TAG, "startAnimation: INSIDE")
+
         with(motionLayout) {
             when (view) {
                 imageview_false -> {
-                    setTransition(R.id.startLeft, R.id.endLeft)
+                    setTransition(R.id.rest, R.id.offScreenInCorrect)
                     transitionToEnd()
                 }
                 imageview_true -> {
-                    setTransition(R.id.start, R.id.end)
+                    setTransition(R.id.rest, R.id.offScreenCorrect)
                     transitionToEnd()
                 }
                 else -> {
